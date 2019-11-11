1b8a0897be192aeb135576c93d9ca64240b14b4e
root <root@EPL428openhab.4loi5y0pvbredmkrawooopecbc.ax.internal.cloudapp.net>
2019-11-11 21:04:26 +0000

    Added click funcitonality for markers, need to add the popup

--- openhab/maps/index.php
+++ openhab/maps/index.php
@@ -1,9 +1,18 @@
 <html><body>
   <div id="mapdiv"></div>
+  <div id="popup" class="ol‐popup">
+      <a href="#" id="popup‐closer" class="ol‐popup‐closer"></a>
+      <div id="popup‐content"></div>
+  </div>
   <script src="http://www.openlayers.org/api/OpenLayers.js"></script>
   <script>
     map = new OpenLayers.Map("mapdiv");
     map.addLayer(new OpenLayers.Layer.OSM());
+    
+    var container = document.getElementById('popup');
+    var content = document.getElementById('popup‐content');
+    var closer = document.getElementById('popup‐closer');
+
 
     var data=<?php	
 		if(!isset($_GET["ID"])){
@@ -46,14 +55,14 @@
             throw new Error("The car specified cannot be found");
     }
 
-    var markers = new OpenLayers.Layer.Markers( "Markers" );
+    var markers = new OpenLayers.Layer.Markers("Markers");
     map.addLayer(markers);    
     var zoom=16;
 
     var i;
     if(data.Locations.length>0){
         for(i=0; i< data.Locations.length; i++){
-    	   var lonLat = new OpenLayers.LonLat([data.Locations[i].Longitude, data.Locations[i].Latitude])
+    	    var lonLat = new OpenLayers.LonLat([data.Locations[i].Longitude, data.Locations[i].Latitude])
                 .transform(
                 new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
 	        map.getProjectionObject() // to Spherical Mercator Projection 
@@ -61,14 +70,28 @@
     	    markers.addMarker(new OpenLayers.Marker(lonLat));		 
 	    if(i===0){
 	        console.log("Car "+data.Name+" has "+data.Locations.length+" locations listed.");
-	        map.setCenter(lonLat, zoom);
 	    }
+	    map.setCenter(lonLat, zoom);
 	}
     }else{
         console.log("No Locations found for car: "+data.Name);
     }
     console.log(data);
-    //console.log(Object.keys(data).length);
-    //console.log(data[0]);
+
+    map.events.register('click',map, function(evt) {
+	var clickCoord = map.getLonLatFromPixel(evt.xy);
+	clickCoord = new OpenLayers.LonLat(clickCoord.lon , clickCoord.lat).transform( new OpenLayers.Projection("EPSG:900913"), new OpenLayers.Projection("EPSG:4326") );	    
+	console.log("You clicked on (lon,lat):" + clickCoord);
+
+	var threshhold = 0.0001;
+	for (var i in markers.markers) {
+	    var markerCoord = new OpenLayers.LonLat(markers.markers[i].lonlat.lon , markers.markers[i].lonlat.lat).transform( new OpenLayers.Projection("EPSG:900913"), new OpenLayers.Projection("EPSG:4326") );	    
+	    //console.log("There is a marker on (lon,lat): "+markerCoord);
+	    if(Math.abs(markerCoord.lon - clickCoord.lon) <threshhold && Math.abs(markerCoord.lat - clickCoord.lat) < threshhold)
+		    console.log("marker clicked!");
+	}
+    });  
+    
+    
   </script>
 </body></html>
