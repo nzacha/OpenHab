a45eed27c657643310af7469225ebd7325859332
root <root@EPL428openhab.4loi5y0pvbredmkrawooopecbc.ax.internal.cloudapp.net>
2019-11-11 17:50:30 +0000

    Added json parsing for multiple cars and decoding for the map presentation:

--- openhab/maps/index.php
+++ openhab/maps/index.php
@@ -5,7 +5,7 @@
     map = new OpenLayers.Map("mapdiv");
     map.addLayer(new OpenLayers.Layer.OSM());
 
-	var results =<?php 
+        var data=<?php 
 		$servername = "localhost";
 		$username = "nzacha";
 		$password = "Password1234!";
@@ -17,34 +17,51 @@
 			die("Connection failed: " . $conn->connect_error);
 		}
 
-		$sql = "Select * FROM CarLocations ORDER BY ID DESC LIMIT 5";
+		$sql = "Select ID, Name FROM Cars ORDER BY ID";
 		$results = $conn->query($sql);
 
 		$retVal = [];
 		if($results->num_rows > 0){
-			$i=0;
-			while($row = $results->fetch_assoc() and $i<5){
-				array_push($retVal, $row);			
-				$i++;
+			while($row = $results->fetch_assoc()){
+				$innerArray = [];
+				$inner_sql = "Select Longitude, Latitude, Timestamp,(CURDATE() - INTERVAL 1 DAY) AS diff FROM CarLocations WHERE Car_ID = ".$row[ID];
+				$inner_results = $conn->query($inner_sql);
+				while($inner_row = $inner_results->fetch_assoc()){
+					array_push($innerArray, $inner_row);	
+				}
+				$object = array(
+					"Name" => $row["Name"] , "Locations" => array_values($innerArray) 				       );
+ 			        array_push($retVal, $object);
 			}
 			echo json_encode($retVal);
 		}
 		$conn->close();
-	?>;
- 
+        ?>;
     var markers = new OpenLayers.Layer.Markers( "Markers" );
     map.addLayer(markers);    
     var zoom=16;
 
-    for(i=0; i<5; i++){
-    var lonLat = new OpenLayers.LonLat([results[i].Latitude,results[i].Longitude])
-          .transform(
-            new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-	    map.getProjectionObject() // to Spherical Mercator Projection 
-    );
-    markers.addMarker(new OpenLayers.Marker(lonLat));
-    if(i===0)      
-        map.setCenter (lonLat, zoom);
-    } 
+    var i,j;
+    for(i=0; i< data.length; i++){
+	if(data[i].Locations.length>0){
+	    for(j=0; j< data[i].Locations.length; j++){
+    	        var lonLat = new OpenLayers.LonLat([data[i].Locations[j].Longitude, data[i].Locations[j].Latitude])
+                    .transform(
+                    new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+	            map.getProjectionObject() // to Spherical Mercator Projection 
+    	            );
+    	        markers.addMarker(new OpenLayers.Marker(lonLat));		
+	    } 
+	    if(i===0){
+		console.log("Car "+data[i].Name+" has "+data[i].Locations.length+" locations listed.");
+	        map.setCenter(lonLat, zoom);
+	    }
+	}else{
+	    console.log("No Locations found for car: "+data[i].Name);
+	}
+    }
+    console.log(data);
+    //console.log(Object.keys(data).length);
+    //console.log(data[0]);
   </script>
 </body></html>
